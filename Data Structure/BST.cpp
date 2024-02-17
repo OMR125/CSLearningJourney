d23f8f1ce5f9acceeec2cc08c2bbb1d0506283d9
Omar Mahfouz <omer12memo1212@gmail.com>
2024-02-17 03:36:51 +0200

    Update BST.cpp
    smart pointers better. less memory usage now and added some methods , organized the code more

--- Data Structure/BST.cpp
+++ Data Structure/BST.cpp
@@ -1,88 +1,145 @@
-#include <bits./stdc++.h>
-using namespace std;
+#include <iostream>
+#include <memory>
+#include <queue>
 
 template <class T>
-struct node {
+struct Node {
     T data;
-    node<T>* left = nullptr;
-    node<T>* right = nullptr;
-    node<T>(T _data) : data(_data){};
+    std::shared_ptr<Node<T>> left = nullptr;
+    std::shared_ptr<Node<T>> right = nullptr;
+
+    // Added explicit keyword to the constructor of Node to prevent implicit conversions.
+    explicit Node(T data) : data(data){};
 };
 
 template <class T>
 class BST {
    private:
-    node<T>* root = nullptr;
+    std::shared_ptr<Node<T>> root = nullptr;
 
-   private:
-    node<T>* insert(node<T>* u, T x) {
-        if (u == nullptr || empty()) {
-            u = new node<T>(x);
-        } else if (x >= u->data) {
-            u->right = insert(u->right, x);
-        } else {
-            u->left = insert(u->left, x);
+    std::shared_ptr<Node<T>> insertNode(std::shared_ptr<Node<T>> node, T data) {
+        if (node == nullptr) {
+            return std::make_shared<Node<T>>(data);
         }
-        return u;
+
+        if (data >= node->data) {
+            node->right = insertNode(node->right, data);
+        } else {
+            node->left = insertNode(node->left, data);
+        }
+
+        return node;
     }
-    node<T>* remove(node<T>* u, T x) {
-        if (u == nullptr) return nullptr;
-        if (x > u->data)
-            u->right = remove(u->right, x);
-        else if (x < u->data)
-            u->left = remove(u->left, x);
-        else {
-            if (u->right == nullptr) {
-                node<T>* temp = u->left;
-                delete u;
-                return temp;
-            } else if (u->left == nullptr) {
-                node<T>* temp = u->right;
-                delete u;
-                return temp;
+
+    std::shared_ptr<Node<T>> removeNode(std::shared_ptr<Node<T>> node, T data) {
+        if (node == nullptr) return nullptr;
+
+        if (data > node->data) {
+            node->right = removeNode(node->right, data);
+        } else if (data < node->data) {
+            node->left = removeNode(node->left, data);
+        } else {
+            if (node->right == nullptr) {
+                return node->left;
+            } else if (node->left == nullptr) {
+                return node->right;
             } else {
-                u->data = max(u->left)->data;
-                u->left = remove(u->left, u->data);
+                node->data = findMax(node->left)->data;
+                node->left = removeNode(node->left, node->data);
             }
         }
-        return u;
+
+        return node;
     }
-    node<T>* search(node<T>* u, T x) {
-        if (x == u->data) return u;
-        if (u == nullptr) return nullptr;
-        if (x > u->data) return search(u->right, x);
-        return search(u->left, x);
+
+    std::shared_ptr<Node<T>> searchNode(std::shared_ptr<Node<T>> node, T data) {
+        if (node == nullptr || data == node->data) return node;
+        
+        if (data > node->data) {
+            return searchNode(node->right, data);
+        }
+        
+        return searchNode(node->left, data);
     }
-    node<T>* max(node<T>* u) {
-        if (u->right == nullptr) return u;
-        return max(u->right);
+
+   private:
+    std::shared_ptr<Node<T>> findMax(std::shared_ptr<Node<T>> node) {
+        if (node->right == nullptr) return node;
+
+        return findMax(node->right);
     }
-    void preOrder(node<T>* u) {
-        if (u == nullptr) return;
-        cout << u->data << " ";
-        preOrder(u->left);
-        preOrder(u->right);
+
+    std::shared_ptr<Node<T>> findMin(std::shared_ptr<Node<T>> node) {
+        if (node->left == nullptr) return node;
+
+        return findMin(node->left);
     }
-    void inOrder(node<T>* u) {
-        if (u == nullptr) return;
-        inOrder(u->left);
-        cout << u->data << " ";
-        inOrder(u->right);
+
+    void printPreOrder(std::shared_ptr<Node<T>> node) {
+        if (node == nullptr) return;
+
+        std::cout << node->data << " ";
+        printPreOrder(node->left);
+        printPreOrder(node->right);
     }
-    void postOrder(node<T>* u) {
-        if (u == nullptr) return;
-        postOrder(u->right);
-        cout << u->data << " ";
-        postOrder(u->left);
+
+    void printInOrder(std::shared_ptr<Node<T>> node) {
+        if (node == nullptr) return;
+
+        printInOrder(node->left);
+        std::cout << node->data << " ";
+        printInOrder(node->right);
+    }
+
+    void printPostOrder(std::shared_ptr<Node<T>> node) {
+        if (node == nullptr) return;
+
+        printPostOrder(node->right);
+        std::cout << node->data << " ";
+        printPostOrder(node->left);
     }
 
    public:
-    bool empty() { return root == nullptr; }
-    void insert(T x) { root = insert(root, x); }
-    void remove(T x) { remove(root, x); }
-    bool search(T x) { return search(root, x); }
-    void preOrder() { preOrder(root); }
+    bool isEmpty() { return root == nullptr; }
+
+    void insert(T data) { root = insertNode(root, data); }
+
+    void remove(T data) { root = removeNode(root, data); }
+
+    bool search(T data) { return searchNode(root, data) != nullptr; }
+
+    T max() { return findMax(root)->data; }
+
+    T min() { return findMin(root)->data; }
+
+    void printInOrder() { printInOrder(root); }
+
+    void printPreOrder() { printPreOrder(root); }
+
+    void printPostOrder() { printPostOrder(root); }
+
+    void BFS() {
+        std::queue<std::shared_ptr<Node<T>>> q;
+        q.push(root);
+
+        while (!q.empty()) {
+            auto node = q.front();
+            std::cout << node->data << "\n";
+            q.pop();
+
+            if (node->right != nullptr) q.push(node->right);
+            if (node->left != nullptr) q.push(node->left);
+        }
+    }
 };
 int main() {
-  
+    BST<int> v;
+    v.insert(6);
+    v.insert(3);
+    v.insert(7);
+    v.insert(11);
+    v.insert(-2);
+    v.insert(9);
+    v.insert(123);
+    v.BFS();
 }
