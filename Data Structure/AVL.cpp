c9a17167d8c0c32c7a1cc4d140af2bfaf85f460c
Omar Mahfouz <omer12memo1212@gmail.com>
2024-02-17 06:22:53 +0200

    some format improvements for AVL.cpp
    renamed variables , added methods . just template needed

--- Data Structure/AVL.cpp
+++ Data Structure/AVL.cpp
@@ -1,178 +1,200 @@
 #include <iostream>
-#include <memory>
 #include <queue>
-
 using namespace std;
 
-struct node {
-    int data;
-    int bf = 0;
-    int depth = 1;
-    node* left = nullptr;
-    node* right = nullptr;
-    node(int _data) : data(_data){};
+struct Node {
+    int value;
+    int balanceFactor = 0;
+    int height = 1;
+    Node* leftChild = nullptr;
+    Node* rightChild = nullptr;
+    Node(int _value) : value(_value){};
 };
 
-class AVL {
+class AVLTree {
    private:
-    node* root = nullptr;
-    int len = 0;
+    Node* rootNode = nullptr;
+    int treeSize = 0;
 
    private:
-    node* insert(node* u, int data) {
-        if (u == nullptr) return new node(data);
-        if (data > u->data)
-            u->right = insert(u->right, data);
+    void updateNode(Node* currentNode) {
+        int rightChildHeight = currentNode->rightChild != nullptr ? currentNode->rightChild->height : 0;
+        int leftChildHeight = currentNode->leftChild != nullptr ? currentNode->leftChild->height : 0;
+        currentNode->height = 1 + max(rightChildHeight, leftChildHeight);
+        currentNode->balanceFactor = leftChildHeight - rightChildHeight;
+    }
+
+    Node* balanceNode(Node* currentNode) {
+        if (currentNode->balanceFactor > 1) { // Left Heavy
+            if (currentNode->leftChild->balanceFactor >= 0) {
+                return rotateRight(currentNode); 
+            } else {
+                currentNode->leftChild = rotateLeft(currentNode->leftChild);
+                return rotateRight(currentNode);
+            }
+        } else if (currentNode->balanceFactor < -1) { // Right Heavy
+            if (currentNode->rightChild->balanceFactor <= 0) {
+                return rotateLeft(currentNode);
+            } else {
+                currentNode->rightChild = rotateRight(currentNode->rightChild);
+                return rotateLeft(currentNode);
+            }
+        }
+        return currentNode;
+    }
+
+    Node* insertNode(Node* currentNode, int value) {
+        if (currentNode == nullptr) return new Node(value);
+        if (value > currentNode->value)
+            currentNode->rightChild =
+                insertNode(currentNode->rightChild, value);
         else
-            u->left = insert(u->left, data);
-        update(u);
-        return balance(u);
+            currentNode->leftChild = insertNode(currentNode->leftChild, value);
+        updateNode(currentNode);
+        return balanceNode(currentNode);
     };
-    node* remove(node* u, int data) {
-        if (u == nullptr) return u;
-        if (data > u->data)
-            u->right = remove(u->right, data);
-        else if (data < u->data)
-            u->left = remove(u->left, data);
+
+    Node* removeNode(Node* currentNode, int value) {
+        if (currentNode == nullptr) return currentNode;
+        if (value > currentNode->value)
+            currentNode->rightChild =
+                removeNode(currentNode->rightChild, value);
+        else if (value < currentNode->value)
+            currentNode->leftChild = removeNode(currentNode->leftChild, value);
         else {
-            if (u->right == nullptr) {
-                return u->left;
-            } else if (u->left == nullptr) {
-                return u->right;
+            if (currentNode->rightChild == nullptr) {
+                return currentNode->leftChild;
+            } else if (currentNode->leftChild == nullptr) {
+                return currentNode->rightChild;
             } else {
-                u->data = FindMax(u->left)->data;
-                u->left = remove(u->left, u->data);
-
-                // also valid method
-                // u->data = min(u->right)->data;
-                // u->right = remove(u->right,u->data);
+                currentNode->value = findMaxNode(currentNode->leftChild)->value;
+                currentNode->leftChild =
+                    removeNode(currentNode->leftChild, currentNode->value);
             }
         }
-        update(u);
-        return balance(u);
+        updateNode(currentNode);
+        return balanceNode(currentNode);
     };
-    node* balance(node* u) {
-        if (u->bf > 1) {  // left heavy
-            if (u->left->bf >= 0) {
-                return R(u);
-            } else {
-                u->left = L(u->left);
-                return R(u);
-            }
-        } else if (u->bf < -1) {  // right heavy
-            if (u->right->bf <= 0) {
-                return L(u);
-            } else {
-                u->right = R(u->right);
-                return L(u);
-            }
-        }
-        return u;
-    }
-    node* search(node* u, int data) {
-        if (u == nullptr || u->data == data) return u;
-        if (data > u->data)
-            return search(u->right, data);
+
+    Node* searchNode(Node* currentNode, int value) {
+        if (currentNode == nullptr || currentNode->value == value)
+            return currentNode;
+        if (value > currentNode->value)
+            return searchNode(currentNode->rightChild, value);
         else
-            return search(u->left, data);
+            return searchNode(currentNode->leftChild, value);
     }
-    node* FindMax(node* u) {
-        if (u->right == nullptr) return u;
-        return FindMax(u->right);
+
+    Node* findMaxNode(Node* currentNode) {
+        if (currentNode->rightChild == nullptr) return currentNode;
+        return findMaxNode(currentNode->rightChild);
     }
-    node* FindMin(node* u) {
-        if (u->left == nullptr) return u;
-        return FindMin(u->left);
+
+    Node* findMinNode(Node* currentNode) {
+        if (currentNode->leftChild == nullptr) return currentNode;
+        return findMinNode(currentNode->leftChild);
     }
-    node* L(node* u) {
-        node* newparent = u->right;
-        u->right = newparent->left;
-        newparent->left = u;
-        update(u);
-        update(newparent);
-        return newparent;
+
+    Node* rotateLeft(Node* currentNode) {
+        Node* newParentNode = currentNode->rightChild;
+        currentNode->rightChild = newParentNode->leftChild;
+        newParentNode->leftChild = currentNode;
+        updateNode(currentNode);
+        updateNode(newParentNode);
+        return newParentNode;
     }
-    node* R(node* u) {
-        node* newparent = u->left;
-        u->left = newparent->right;
-        newparent->right = u;
-        update(u);
-        update(newparent);
-        return newparent;
+
+    Node* rotateRight(Node* currentNode) {
+        Node* newParentNode = currentNode->leftChild;
+        currentNode->leftChild = newParentNode->rightChild;
+        newParentNode->rightChild = currentNode;
+        updateNode(currentNode);
+        updateNode(newParentNode);
+        return newParentNode;
     }
-    void update(node* u) {
-        int op1 = u->right != nullptr ? u->right->depth : 0;
-        int op2 = u->left != nullptr ? u->left->depth : 0;
-        u->depth = 1 + max(op1, op2);
-        u->bf = op2 - op1;
+
+    void inOrderTraversal(Node* currentNode) {
+        inOrderTraversal(currentNode->leftChild);
+        cout << currentNode->value << "\n";
+        inOrderTraversal(currentNode->rightChild);
     }
-    void* inOrder(node* u) {
-        inOrder(u->left);
-        cout << u->data << "\n";
-        inOrder(u->right);
+
+    void preOrderTraversal(Node* currentNode) {
+        cout << currentNode->value << "\n";
+        preOrderTraversal(currentNode->leftChild);
+        preOrderTraversal(currentNode->rightChild);
     }
-    void* preOrder(node* u) {
-        cout << u->data << "\n";
-        preOrder(u->left);
-        preOrder(u->right);
-    }
-    void* postOrder(node* u) {
-        postOrder(u->right);
-        cout << u->data << "\n";
-        postOrder(u->left);
+
+    void postOrderTraversal(Node* currentNode) {
+        postOrderTraversal(currentNode->rightChild);
+        cout << currentNode->value << "\n";
+        postOrderTraversal(currentNode->leftChild);
     }
 
    public:
-    bool insert(int data) {
-        if (search(data)) return false;
-        root = insert(root, data);
-        len++;
+    // operations on the tree
+    bool insertNode(int value) {
+        if (searchNode(value)) return false;
+        rootNode = insertNode(rootNode, value);
+        treeSize++;
         return true;
     };
-    void remove(int data) { root = remove(root, data); };
-    bool search(int data) { return search(root, data); };
-    bool empty() { return root == nullptr; };
-    int size() { return len; }
-    int depth(int data) {
-        node* distination = search(root, data);
-        return distination != nullptr ? distination->depth - 1 : -1;
+
+    void removeNode(int value) { rootNode = removeNode(rootNode, value); };
+
+    bool searchNode(int value) { return searchNode(rootNode, value); };
+
+    // size related stuff
+    bool isEmpty() { return rootNode == nullptr; };
+
+    int getSize() { return treeSize; }
+
+    int getNodeDepth(int value) {
+        Node* destinationNode = searchNode(rootNode, value);
+        return destinationNode != nullptr ? destinationNode->height - 1 : -1;
     }
+
+    // Queries
+    int getMaxElement() { return findMaxNode(rootNode)->value; }
+    int getMinElement() { return findMinNode(rootNode)->value; }
+
+    // Traversals
     void BFS() {
-        queue<node*> q;
-        q.push(root);
+        queue<Node*> q;
+        q.push(rootNode);
         while (!q.empty()) {
-            int size = q.size();
-            while (size--) {
+            int curSize = q.size();
+            while (curSize--) {
                 auto node = q.front();
-                cout << node->data << "\n";
+                cout << node->value << "\n";
                 q.pop();
-                if (node->right != nullptr) q.push(node->right);
-                if (node->left != nullptr) q.push(node->left);
+                if (node->rightChild != nullptr) q.push(node->rightChild);
+                if (node->leftChild != nullptr) q.push(node->leftChild);
             }
             cout << "\n";
         }
     }
-    int max_element() { return FindMax(root)->data; }
-    int min_element() { return FindMin(root)->data; }
-    void inOrder() { inOrder(root); }
-    void preOrder() { preOrder(root); }
-    void postOrder() { postOrder(root); }
+    void inOrderTraversal() { inOrderTraversal(rootNode); }
+
+    void preOrderTraversal() { preOrderTraversal(rootNode); }
+
+    void postOrderTraversal() { postOrderTraversal(rootNode); }
 };
 int main() {
-    AVL x;
-    x.insert(1);
-    x.insert(2);
-    x.insert(3);
-    x.insert(4);
-    x.insert(5);
-    x.remove(3);
-    x.insert(99);
-    x.insert(-5);
-    x.insert(72);
-    x.insert(52);
-    x.insert(0);
-    x.insert(-3);
-    x.remove(4);
-    x.remove(3);
+    AVLTree x;
+    x.insertNode(1);
+    x.insertNode(2);
+    x.insertNode(3);
+    x.insertNode(4);
+    x.insertNode(5);
+    x.removeNode(3);
+    x.insertNode(99);
+    x.insertNode(-5);
+    x.insertNode(72);
+    x.insertNode(52);
+    x.insertNode(0);
+    x.insertNode(-3);
+    x.removeNode(4);
+    x.removeNode(3);
     x.BFS();
 }
