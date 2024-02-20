7ac0e13f75b4afc2e6065df58b6c44f85c79f62c
Omar Mahfouz <omer12memo1212@gmail.com>
2024-02-20 20:34:19 +0200

    Update PointUpdate RangeSum.cpp

--- Data Structure/Fenwick Tree /PointUpdate RangeSum.cpp
+++ Data Structure/Fenwick Tree /PointUpdate RangeSum.cpp
@@ -5,33 +5,33 @@ template <typename T>
 class Fenwick {
    private:
     int n;
-    vector<int> v;
+    vector<T> Tree;
 
    public:
     Fenwick(int n = 0) { init(n); }
     void init(int _n) {
         n = _n + 1;
-        v.assign(n, T{});
+        Tree.assign(n, T{});
     }
-    void build(vector<int> &arr) {
+    void build(vector<T> &arr) {
         int n = arr.size();
         init(n);
         for (int i = 0; i < n; i++) update(i, arr[i]);
     }
-    void update(int i, T x) {
-        for (++i; i <= n; i += i & -i) v[i] += x;
+    void update(int i, T value) {
+        for (++i; i <= n; i += i & -i) Tree[i] += value;
     }
     T sum(int i) {
         T ans{};
-        for (++i; i > 0; i -= i & -i) ans += v[i];
+        for (++i; i > 0; i -= i & -i) ans += Tree[i];
         return ans;
     }
     T range_sum(int l, int r) { return sum(r) - sum(l - 1); }
 };
 int main() {
-    Fenwick<int> F;
-    vector<int> arr(10);
-    for (int i = 0; i < 10; i++) arr[i] = i + 1;
+    Fenwick<double> F;
+    vector<double> arr(10);
+    for (int i = 0; i < 10; i++) arr[i] = i;
     F.build(arr);
     // 1 2 3 4 5 6 7 8 9 10
     cout << F.range_sum(4, 7) << "\n";
