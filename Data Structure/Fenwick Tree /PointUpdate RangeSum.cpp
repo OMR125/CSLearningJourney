70a17ebcf8d0843e68725ec101d1513b74620ce5
Omar Mahfouz <omer12memo1212@gmail.com>
2024-02-20 20:54:16 +0200

    Update PointUpdate RangeSum.cpp

--- Data Structure/Fenwick Tree /PointUpdate RangeSum.cpp
+++ Data Structure/Fenwick Tree /PointUpdate RangeSum.cpp
@@ -14,8 +14,7 @@ class Fenwick {
         Tree.assign(n, T{});
     }
     void build(vector<T> &arr) {
-        int n = arr.size();
-        init(n);
+        init(arr.size());
         for (int i = 0; i < n; i++) update(i, arr[i]);
     }
     void update(int i, T value) {
