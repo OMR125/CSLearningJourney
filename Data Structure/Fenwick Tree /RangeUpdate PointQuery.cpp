ae8da51b6e53b5bba9a03023f330f472f74d7739
Omar Mahfouz <omer12memo1212@gmail.com>
2024-02-20 20:31:55 +0200

    Update RangeUpdate PointQuery.cpp

--- Data Structure/Fenwick Tree /RangeUpdate PointQuery.cpp
+++ Data Structure/Fenwick Tree /RangeUpdate PointQuery.cpp
@@ -2,39 +2,38 @@
 
 using namespace std;
 
-template <typename T>
 class Fenwick {
    private:
     int n;
-    vector<int> v;
+    vector<int> Tree;
 
    public:
     Fenwick(int n = 0) { init(n); }
     void init(int _n) {
         n = _n + 1;
-        v.assign(n, T{});
+        Tree.assign(n, 0);
     }
     void build(vector<int> &arr) {
         int n = arr.size();
         init(n);
         for (int i = 0; i < n; i++) range_update(i, i, arr[i]);
     }
-    void add(int i, T x) {
-        for (++i; i <= n; i += i & -i) v[i] += x;
+    void add(int i, int value) {
+        for (++i; i <= n; i += i & -i) Tree[i] += value;
     }
-    void range_update(int l, int r, int x) {
-        add(l, x);
-        add(r + 1, -x);
+    void range_update(int l, int r, int value) {
+        add(l, value);
+        add(r + 1, -value);
     }
-    T sum(int i) {
-        T ans{};
-        for (++i; i > 0; i -= i & -i) ans += v[i];
+    int sum(int i) {
+        int ans = 0;
+        for (++i; i > 0; i -= i & -i) ans += Tree[i];
         return ans;
     }
-    T Query(int i) { return sum(i); }
+    int Query(int i) { return sum(i); }
 };
 int main() {
-    Fenwick<int> F;
+    Fenwick F;
     vector<int> arr(10);
     for (int i = 0; i < 10; i++) arr[i] = i + 1;
     F.build(arr);
